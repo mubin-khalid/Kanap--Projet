dda047a7f04741e573d872ddebea242ce3dcc53a
Brice789 <kireche.brice@gmail.com>
2022-11-26 22:54:05 +0100

    debut

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,18 @@
-alert("hello")
+// alert("hello")
+
+//api de chrome mais qu'en est t'il de fixefox ou safari?
+
+
+fetch("http://localhost:3000/api/products")
+    .then((oui) => oui.json())
+    .then((data) => data.console.log())
+
+
+const  first = document.createElement("Monpremierelement")
+// first.href = "http://localhost:3000/api/product"
+first.href ="http://localhost:3000/images/kanap02.jpeg"
+
+first.text = "description de l'image"
+
+
+const mapremierimage = document.querySelector("#jay")
