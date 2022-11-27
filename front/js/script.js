ce619b010758b1c7157cb9b7f1d416c3c06d7413
Brice789 <kireche.brice@gmail.com>
2022-11-27 12:42:22 +0100

    fait un lien qui lie la premier image de tab

--- front/js/script.js
+++ front/js/script.js
@@ -5,14 +5,36 @@
 
 fetch("http://localhost:3000/api/products")
     .then((oui) => oui.json())
-    .then((data) => data.console.log())
+    .then((data) => {
+        console.log(data)
+        const imageUrl = data[0].imageUrl
+        //console.log("url display, j'afiche l'imahe du tableau 8 images existe", imageUrl)
+   
 
 
-const  first = document.createElement("Monpremierelement")
+   const first = document.createElement("a") //creer du HTML 
 // first.href = "http://localhost:3000/api/product"
-first.href ="http://localhost:3000/images/kanap02.jpeg"
+// first.href ="http://localhost:3000/images/kanap02.jpeg"
 
-first.text = "description de l'image"
+   first.href = imageUrl
+
+  first.text = "description de l'image du kanap"
+
+///Comment selection un element? On peut regarder dans l'API de Chrome 
+/// On peut peut etre utliser document.querySelector. On selection la div Item ou L'ID items directement. (On voit qu'on à bien implemeter la fonction
+// directent sur la console en ligne. On selection l'ID ou le nom de vid avec un # ET ENTRE " " )
 
 
-const mapremierimage = document.querySelector("#jay")
+
+  const mapremierimage = document.querySelector("#items")
+
+
+
+//cedant  mapremierimage et first sont séparer. Dans le HTML les  deux sont imbriqué
+
+//J'ai chercher alors et la foncton append child peut marcher
+
+
+  mapremierimage.appendChild(first)
+
+})
