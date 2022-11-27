b4ade6896da201647297b23c9d92013e6cf1312a
Brice789 <kireche.brice@gmail.com>
2022-11-27 23:15:45 +0100

    se procurer l'id des images

--- front/js/script.js
+++ front/js/script.js
@@ -5,42 +5,52 @@
 
 fetch("http://localhost:3000/api/products")
     .then((oui) => oui.json())
-    .then((data) => add(data))
+    .then((data) => {
+        return add(data)
+    })
 
 
 function add(donne){
+    const id = data[0]._id
+    first.text = "description de l'image du kanap"
+    const urlfonction = balise(imageUrl)
+    const article = fairearticle()
+    appendChild(balise)
 
+
+}
         
-        console.log(data)
-        
-        const imageUrl = data[0].imageUrl
-        //console.log("url display, j'afiche l'imahe du tableau 8 images existe", imageUrl)
-   
-
-
-   const first = document.createElement("a") //creer du HTML 
+// console.log(data)
+//console.log("url display, j'afiche l'imahe du tableau 8 images existe", imageUrl)
+/// const first = document.createElement("a") //creer du HTML 
 // first.href = "http://localhost:3000/api/product"
 // first.href ="http://localhost:3000/images/kanap02.jpeg"
-
-   first.href = imageUrl
-
-  first.text = "description de l'image du kanap"
-
+  
 ///Comment selection un element? On peut regarder dans l'API de Chrome 
 /// On peut peut etre utliser document.querySelector. On selection la div Item ou L'ID items directement. (On voit qu'on à bien implemeter la fonction
 // directent sur la console en ligne. On selection l'ID ou le nom de vid avec un # ET ENTRE " " )
-
-
-
-  const mapremierimage = document.querySelector("#items")
-
-
-
 //cedant  mapremierimage et first sont séparer. Dans le HTML les  deux sont imbriqué
-
 //J'ai chercher alors et la foncton append child peut marcher
 
+function balise(url){
+    const first = document.createElement("a") //creer du HTML 
+    first.href = "./product.html?id=42" //+ id
+    first.text = "description de l'image du kanap"
+    return urlfonction
+}
 
-  mapremierimage.appendChild(first)
+function appendChild(urlfonction){
+    const mapremierimage = document.querySelector("#items")
+    if(items != null)
+    {
+        mapremierimage.appendChild(first)
+    }
+    first.text = "description de l'image du kanap"
+}
+
+
+function fairearticle(){
+    const article = document.createElement("article") //creer du HTML 
+    return article
 
 }
