3fb65e60ea6f3e5832c42f1f94b459aa32b5408e
Brice789 <kireche.brice@gmail.com>
2023-01-12 18:48:06 +0100

    Update 77

--- front/js/script.js
+++ front/js/script.js
@@ -44,25 +44,3 @@ function child(balisea, article, img, h3, p){
 
 
 
-
-// altTxt
-// : 
-// "Photo d'un canapé bleu, deux places"
-// colors
-// : 
-// (3) ['Blue', 'White', 'Black']
-// description
-// : 
-// "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-// imageUrl
-// "http://localhost:3000/images/kanap01.jpeg"
-
-// name
-// "Kanap Sinopé"
-
-// price
-// 1849
-
-// _id
-// "107fb5b75607497b96722bda5b504926"
